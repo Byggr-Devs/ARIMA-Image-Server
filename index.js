e7462236da417bb9ede154b0183c1842872ed857
Raveen2001 <raveenanbarasan@gmail.com>
2022-01-11 00:37:27 +0530

    commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 console.log(__dirname);
 //setting middleware
-app.use(express.static(__dirname + "/images")); //Serves resources from public folder
+app.use(express.static(__dirname + "/images"));
 app.listen(5000, () => {
   console.log("Images are serving at port 5000");
 });
